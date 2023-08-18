06e6f3083bc74c87b879b1ced45b8afe56ff65ea
Ahmed Raza <ahmedraza6100@gmail.com>
2023-08-18 23:16:43 +0500

    Created and integrated GET Api

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,13 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cartColumns, db } from "../../../../Database/Drizzle";
+import { cookies } from "next/headers";
+import { v4 } from "uuid";
+import { eq } from "drizzle-orm";
 
 export const POST = async (request: NextRequest) => {
   const req = await request.json();
+  const setCookies = cookies();
+  const uid = v4();
+  const user_id = setCookies.get("user_id")?.value as string;
+  if (!user_id) {
+    setCookies.set("user_id", uid);
+  }
   try {
     const res = await db
       .insert(cartColumns)
       .values({
-        user_id: req.user_id,
+        user_id: user_id,
         product_id: req.product_id,
         product_title: req.product_title,
         image_url: req.image_url,
@@ -28,5 +37,19 @@ export const POST = async (request: NextRequest) => {
     console.log("Error While Posting Data To Database");
     console.log("error", error);
     return NextResponse.json({ message: "Something Went Wrong" });
+  }
+};
+
+export const GET = async (request: NextRequest) => {
+  const uid = request.nextUrl.searchParams.get("user_id") as string;
+  try {
+    const res = await db
+      .select()
+      .from(cartColumns)
+      .where(eq(cartColumns.user_id, uid));
+    return NextResponse.json(res);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error);
   }
 };
