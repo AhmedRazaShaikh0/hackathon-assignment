45977649c60b1fbcc38909b2e1757c5d2893e9c7
Ahmed Raza <ahmedraza6100@gmail.com>
2023-08-23 14:00:03 +0500

    implemented Clerk for login signup and for apis

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,22 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cartColumns, db } from "../../../../Database/Drizzle";
-import { cookies } from "next/headers";
-import { v4 } from "uuid";
 import { eq } from "drizzle-orm";
 
 export const POST = async (request: NextRequest) => {
   const req = await request.json();
-  const setCookies = cookies();
-  const uid = v4();
-  const user_id = setCookies.get("user_id")?.value as string;
-  if (!user_id) {
-    setCookies.set("user_id", uid);
-  }
+
   try {
     const res = await db
       .insert(cartColumns)
       .values({
-        user_id: user_id,
+        user_id: req.user_id,
         product_id: req.product_id,
         product_title: req.product_title,
         image_url: req.image_url,
